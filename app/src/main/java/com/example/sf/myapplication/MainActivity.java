5a405796e8d56e89a7eda4a37d904ea70e93c3cc
krystenia <dlf_8912@163.com>
2017-03-21 11:33:03 +0800

    add something

--- app/src/main/java/com/example/sf/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sf/myapplication/MainActivity.java
@@ -5,12 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button login;
+    private EditText usernameEt,pwdEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,27 +31,45 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        init();
+    }
+
+    private void init(){
+        login= (Button) findViewById(R.id.button);
+        usernameEt= (EditText) findViewById(R.id.username);
+        pwdEt= (EditText) findViewById(R.id.password);
+        login.setOnClickListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String name= usernameEt.getText().toString();
+        String pwd=pwdEt.getText().toString();
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this,"username must not be empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pwd)){
+            Toast.makeText(this,"password must not be empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
